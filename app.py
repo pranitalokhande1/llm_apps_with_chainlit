f78cffd4ea099b99620cea929fb134085da3f357
pranitalokhande1 <pranitalokhande2@gmail.com>
2025-05-26 17:18:05 +0530

    chnages svaed

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import chainlit as cl
+from src.llm import ask_order, messages
 
 @cl.on_message
-async def main(message:cl.message):
+async def main(message: cl.Message):
+    messages.append({"role": "user", "content": message.content})
+    response = ask_order(messages)  # No await needed since ask_order is synchronous
+    messages.append({"role": "assistant", "content": response})
 
-    await cl.message(
-        content=f"Received:{message.content}",
-    ).send()
+    await cl.Message(content=response).send()
--- src/llm.py
+++ src/llm.py
@@ -1,18 +1,12 @@
 from openai import OpenAI
-from src.prompt import system_instruction
+import os
 
 client = OpenAI()
 
-messages = [
-    {"role": "system", "content": system_instruction}
-]
-
-
-def ask_order(messages, model="gpt-3.5-turbo",temperature=0):
+def ask_order(messages, model="gpt-3.5-turbo", temperature=0):
     response = client.chat.completions.create(
-        model= model,
-        messages=  messages,
-        temperature= temperature 
+        model=model,
+        messages=messages,
+        temperature=temperature
     )
-
     return response.choices[0].message.content
